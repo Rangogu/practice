d5d57391dfbbe28d66b46f35ff7a163760a97873
Rangogu <boresahkemi@gmail.com>
2024-12-26 19:12:42 +0000

    bbbb

---
+++ you.py
@@ -0,0 +1,10 @@
+class fruit:
+    def __init__(self,name,color):
+        self.name=name
+        self.color=color
+
+    def intro(self):
+        print("i love", self.name)
+apple=fruit("red")
+print(apple.intro)
+        
